cf33376535ab4d8a259cccf9616ee19930e08650
carlos505 <aragonaugusto24@gmail.com>
2023-07-17 12:32:57 -0600

    Fix checkWinner function so that it can announce the right winner

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const game = (function() {
     const squareEl = document.querySelectorAll('.square');
-    const announceWinnerEl = document.querySelector('#announce-winner');
+    const announceWinnerEl = document.getElementById('announce-winner');
     const winnerWrapperEl = document.querySelector('.winner-wrapper');
     const playAgainBtn = document.querySelector('.play-again-button');
     const gameStatusEl = document.querySelector('.game-status');
@@ -17,8 +17,6 @@ const game = (function() {
 
     const state = {
         isUserTurn: true,
-        userMark: 'x',
-        aiMark: 'o',
         userScore: 0,
         aiScore: 0,
         gameTie: 0,
@@ -29,91 +27,83 @@ const game = (function() {
 
     function handleSquareClick(event) {
         const square = event.target.closest('.square');
-        if (square && square.innerHTML === '' && state.isUserTurn) {
-            makeUserMove(square);
-            setTimeout(() => {
-                aiTurn();
-                checkWinner();
-            }, 500);
+        if (square && square.innerHTML === '' && state.isUserTurn === true) {
+            if (state.isUserTurn) {
+                gameStatusEl.style.display = 'none';
+                makeMove(square, 'x', 'cross');
+                state.isUserTurn = false;
+                setTimeout(() => {
+                    aiTurn();
+                    checkWinner();
+                }, 500);    
+            };
         };
     };
 
-    function makeUserMove(square) {
-        if (state.userMark === 'x') {
-            gameStatusEl.style.display = 'none';
-            const img = document.createElement('img');
-            img.src = `./images/${state.userMark}.png`;
-            img.width = '90px';
-            img.classList.add('cross-image', 'cross');
-            square.textContent = '';
-            square.appendChild(img)
-            state.isUserTurn === false;
-        }
+    function makeMove(square, mark, cls) {
+        square.innerHTML = `<img src="./images/${mark}.png" width="90px" class="${cls}-image ${cls}">`;
     };
 
     function aiTurn() {
         if (!state.isUserTurn) {
-            const boardEl = document.querySelector('board');
-            const emptySquares = [...boardEl.children].filter(square => square.innerHTML === '');
+            const emptySquares = [...squareEl].filter(square => square.innerHTML === '');
     
             if (emptySquares.length > 0) {
-                    const randomIndex = Math.floor( Math.random() * emptySquares.length );
-                    const chosenSquare = emptySquares[randomIndex];
-                    if (state.aiMark === 'o') {
-                        const img = document.createElement('img');
-                        img.src = `./images/${state.aiMark}.png`;
-                        img.width = '90px';
-                        img.classList.add('circle-image', 'circle');
-                        chosenSquare.textContent = '';
-                        chosenSquare.appendChild(img);
-                        state.isUserTurn = true;    
-                    };
+                const randomIndex = Math.floor( Math.random() * emptySquares.length );
+                const chosenSquare = emptySquares[randomIndex];
+                makeMove(chosenSquare, 'o', 'circle');
+                state.isUserTurn = true;
             };
-
         };
-    };
+    }; // Handles the AI selection
 
     function checkWinner() { // Checks who the winner is and prints out the result
+        const board = [...squareEl].map(square => square.innerHTML);
+        // console.log({ board })
+    
         for (let condition of winningConditions) {
             const [a, b, c] = condition;
-            const squareA = squareEl[a].innerHTML;
-            const squareB = squareEl[b].innerHTML;
-            const squareC = squareEl[c].innerHTML;
+            const squareA = board[a];
+            const squareB = board[b];
+            const squareC = board[c];
 
-            if (squareA === squareB && squareB === squareC && squareA !== '') {
-                if (squareA.includes(state.userMark)) {
+            console.log({squareA})
+
+            if (squareA !== '' && squareA === squareB && squareB === squareC) {
+                if (squareA.includes('cross') && squareB.includes('cross')) {
                     state.userScore++;
                     state.currentGameRound++;
                     showRoundResult('You win!');
                     updateScore(userScoreEl, state.userScore);
                     updateScore(gameRoundEl, state.currentGameRound);
 
-                } else if (squareA.includes(state.aiMark)) {
+                } else if (squareA.includes('circle') && squareB.includes('circle')) {
                     state.aiScore++;
                     state.currentGameRound++;
-                    showRoundResult('Computer wins!');
+                    showRoundResult('Computer Wins!');
                     updateScore(aiScoreEl, state.aiScore);
                     updateScore(gameRoundEl, state.currentGameRound);
-                };
+                }
                 resetGame();
 
-                if (state.userScore === 3 || state.aiScore === 3) {
+                if (state.userScore === 5 || state.aiScore === 5) {
                     setTimeout(() => {
                         announceFinalResult();
                     }, 500);
                 };
                 return;
+                
             };
         };
 
         if ([...squareEl].every(square => square.innerHTML !== '')) {
             state.gameTie++;
             state.currentGameRound++;
-            showRoundResult(`It's a tie!`)
-            updateScore(gameTieEl, state.gameTie)
+            showRoundResult(`It's a tie!`);
+            updateScore(gameTieEl, state.gameTie);
             updateScore(gameRoundEl, state.currentGameRound);
             resetGame();
-        };
+        };    
     };
 
     function showRoundResult(message) {
@@ -121,8 +111,8 @@ const game = (function() {
         gameStatusEl.innerHTML = `<h2>${message}</h2>`;
     };
 
-    function updateScore(element, score) {
-        element.innerHTML = `<span>${score}</span>`;
+    function updateScore(element, message) {
+        element.innerHTML = `<span>${message}</span>`;
     };
 
     function announceFinalResult() {
@@ -146,7 +136,8 @@ const game = (function() {
     function resetGame() {
         squareEl.forEach(e => {
             e.innerHTML = '';
-        });
+        })
+        state.isUserTurn = true;
     };
 
     function playAgain() {
